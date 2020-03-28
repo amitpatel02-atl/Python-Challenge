40a28678ab593c83dcdb78be61d4c18c51fae6a6
amitpatel02-atl <61107007+amitpatel02-atl@users.noreply.github.com>
2020-03-28 00:14:23 -0400

    Update main.py
    update code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,9 @@
-# Modules
+# First we'll import the os module
+# This will allow us to create file paths across operating systems
 import os
+
+# Module for reading CSV files
 import csv
+
+# Set path for file
+csvpath = os.path.join("..", "Resources", "election_data.csv")
