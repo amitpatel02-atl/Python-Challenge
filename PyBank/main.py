1efb6cc64c7f714e6f9069567a1a4199e64785ab
amitpatel02-atl <61107007+amitpatel02-atl@users.noreply.github.com>
2020-03-29 21:57:55 -0400

    Update main.py
    update code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,18 @@
-# First we'll import the os module
-# This will allow us to create file paths across operating systems
+#First we'll import the os module
+#This will allow us to create file paths across operating systems
 import os
 
-# Module for reading CSV files
+#Module for reading CSV files
 import csv
 
-# Set path for file
+#Set path for file
 csvpath = os.path.join("budget_data.csv")
 
 #Open the csv
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # Read the header row first (skip this step if there is now header)
+    #Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
@@ -21,17 +21,17 @@ with open(csvpath) as csvfile:
     Greatest_Increase = 0
     Greatest_Decrease = 0
 
-    # Loop through looking for the months then add to counter
+    #Loop through looking for the months then add to counter
     month_counter = 0
     for row in csvreader:
 
-       # Count number of months 
+       #Count number of months 
        month_counter= (month_counter+ 1)
 
-       # Calculate Total Profit
+       #Calculate Total Profit
        total_profit = total_profit + int(row[1])
 
-       # Calculate month-to-month change
+       #Calculate month-to-month change
        if month_counter ==1:
            previous = int(row[1])
        else:
@@ -39,7 +39,7 @@ with open(csvpath) as csvfile:
             previous = int(row[1])
             List_of_changes.append(month_to_month_change)
 
-            # Calculate greatest increase in profits:
+            #Calculate greatest increase in profits:
             if Greatest_Increase < month_to_month_change:
                 Greatest_Increase = month_to_month_change
                 Greatest_Increase_Date = str(row[0])
@@ -47,12 +47,20 @@ with open(csvpath) as csvfile:
             elif Greatest_Decrease > month_to_month_change:
                 Greatest_Decrease = month_to_month_change
                 Greatest_Decrease_Date = str(row[0])
+    
+    #Set variable for output file
+    output_file = os.path.join("budget_data.csv")
 
+    #  Open the output file
+    with open(output_file, "w") as datafile:
+
+        # Write the header row
+        writer.writerow(["Financial Analysis"])
     print("Financial Analysis")
     print("------------------")
     print(f"Total Months : {month_counter}")
     print(f"Total : ${total_profit}")
-    print(f"Average Change: float("{0:.2f}".format{sum(List_of_changes)/{month_counter} - 1)")
-    print(f"Greatest Increase in Profits : {Greatest_Increase_Date} {Greatest_Increase}")
-    print(f"Greatest Decrease in Profits : {Greatest_Decrease_Date} {Greatest_Decrease}")
+    print(f"Average Change:{sum(List_of_changes)/ (month_counter-1)}")
+    print(f"Greatest Increase in Profits : {Greatest_Increase_Date} (${Greatest_Increase})")
+    print(f"Greatest Decrease in Profits : {Greatest_Decrease_Date} (${Greatest_Decrease})")
    
