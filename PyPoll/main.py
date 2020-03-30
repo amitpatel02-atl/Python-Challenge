41d4b44b5efbf795eea97ff5aa166264a8f4fa8c
amitpatel02-atl <61107007+amitpatel02-atl@users.noreply.github.com>
2020-03-29 22:32:52 -0400

    Update main.py
    Update code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,4 +6,15 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "election_data.csv")
+csvpath = os.path.join("election_data.csv")
+
+#Open the csv
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    #Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    total_votes = 0
+    
